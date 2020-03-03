4a9581478ae1139442b64c91e57ab3c27879cbb9
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-03 12:19:22 -0500

    Update myColor.java

--- myColor.java
+++ myColor.java
@@ -1,10 +1,8 @@
-package sample;
-
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public enum myColor {
-    RED(255,0,0), GREEN(0,255,0), BLUE(0,0,255), WHITE(255,255,255);
+    RED(255,0,0), GREEN(0,255,0), BLUE(0,0,255), WHITE(255,255,255), BLACK(0,0,0), YELLOW(255,255,0), PINK(255, 192,203), MAGENTA(139,0,139);
 
     int r,g,b;
 

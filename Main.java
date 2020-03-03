1db53d973b09f534befe3a5eeee54a4e726e95f6
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-03 12:21:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,13 +20,28 @@ public class Main extends Application {
         myShape c1 = new myShape(500,500, myColor.BLUE);
         Canvas canvas = new Canvas(c1.getX(), c1.getY());
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        myLine l1 = new myLine(0,0, 250, 250, myColor.BLUE);
-        myPolygon p1 = new myPolygon(250,250,0,0,6,50, myColor.GREEN);
-        myCircle cir1 = new myCircle(250,250,0,0,10, myColor.RED);
-        c1.draw(gc);
-        l1.draw(gc);
+        myLine l1 = new myLine(0,0, 500, 500, myColor.BLACK);
+        myLine l2 = new myLine(5,5, 500, 5, myColor.BLACK);
+        myLine l3 = new myLine(5,5, 5, 500, myColor.BLACK);
+        myLine l4 = new myLine(500,0, 0, 500, myColor.BLACK);
+
+        myPolygon p1 = new myPolygon(250,250,0,0,6,200, myColor.BLACK);
+        myCircle cir1 = new myCircle(200,200,0,0,100, myColor.YELLOW);
+        myCircle cir2 = new myCircle(225,225,0,0,50, myColor.PINK);
+        myPolygon p2 = new myPolygon(250,250,0,0,5,160, myColor.GREEN);
+        myPolygon p3 = new myPolygon(250,250,0,0,6,110, myColor.BLUE);
+
         p1.draw(gc);
         cir1.draw(gc);
+        p2.draw(gc);
+        cir2.draw(gc);
+        p3.draw(gc);
+
+        l1.draw(gc);
+        l2.draw(gc);
+        l3.draw(gc);
+        l4.draw(gc);
+
 
         Pane P = new Pane();
         P.getChildren().add(canvas);

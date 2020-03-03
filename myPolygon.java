cd3ed619b977a171f6ff677958311e052e389a42
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-03 12:20:20 -0500

    Update myPolygon.java

--- myPolygon.java
+++ myPolygon.java
@@ -1,11 +1,11 @@
-package sample;
-
 import javafx.scene.canvas.GraphicsContext;
 import java.lang.Math;
 import javafx.scene.shape.Polygon;
 
 public class myPolygon extends myShape{
     private int x, y, n, r;
+    double[] Xcorrds;
+    double[] Ycorrds;
 
     public myPolygon(int x1, int y1, int x, int y, int n, int r, myColor c) {
         super(x, y, c);
@@ -13,19 +13,31 @@ public class myPolygon extends myShape{
         this.y = y1;
         this.n = n;
         this.r = r;
+        this.Xcorrds = new double[n];
+        this.Ycorrds = new double[n];
+
+        for(int i = 0; i < n; i++){
+            this.Xcorrds[i] = r* Math.cos(2*Math.PI*i/n) + this.x;                    //((Math.sin((double)i/(n*2*Math.PI)))* r) + this.x;
+            System.out.println(Xcorrds[i]);
+            this.Ycorrds[i] = r* Math.sin(2*Math.PI*i/n) + this.y;                    //((Math.cos((double)i/(n*2*Math.PI)))* r) + this.y;
+            System.out.println(Ycorrds[i]);
+        }
+    }
+    public double getPerimeter(myPolygon poly){
+        return getSides(poly)*this.n;
+    }
+    public double getSides(myPolygon poly){
+        return Math.sqrt((Xcorrds[0]*Xcorrds[0]) + (Ycorrds[0]*Ycorrds[0]));
+    }
+    public double getArea(myPolygon poly){
+        return ((getSides(poly)/(2*Math.tan(180/this.n)))*getPerimeter(poly))/2;
     }
 
     @Override
     public void draw(GraphicsContext gc) {
         //Polygon p1 = new Polygon();
-        double[] Xcorrds = new double[n];
-        double[] Ycorrds = new double[n];
-       for(int i = 0; i < n; i++){
-            Xcorrds[i] = r* Math.cos(2*Math.PI*i/n) + this.x;                    
-            System.out.println(Xcorrds[i]);
-            Ycorrds[i] = r* Math.sin(2*Math.PI*i/n) + this.y;                    
-            System.out.println(Ycorrds[i]);
-        }
+        //double[] Xcorrds = new double[n];
+        //double[] Ycorrds = new double[n];
         //gc.fillPolygon(Xcorrds,  Ycorrds, n);
         gc.setFill(getC().getARGB());
         gc.fillPolygon(Xcorrds, Ycorrds, n);

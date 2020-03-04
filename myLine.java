10d1b01384146537d6cba1990c766bbb0a659444
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-03 22:41:26 -0500

    Update myLine.java

--- myLine.java
+++ myLine.java
@@ -1,3 +1,4 @@
+
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.Line;
 
@@ -6,6 +7,7 @@ import static java.lang.Math.sqrt;
 
 public class myLine extends myShape{
     private int EndX, EndY;
+    private double length, angle;
 
     public myLine(int x, int y, int endX, int endY, myColor c) {
         super(x, y, c);
@@ -13,21 +15,34 @@ public class myLine extends myShape{
         EndY = endY;
     }
 
-    public double getLength(myLine line){
+    public void getLength(myLine line){
         double x1 = line.getX();
         double x2 = this.EndX;
         double y1 = line.getY();
         double y2 = this.EndY;
         //double totallength = sqrt(pow((x2 - x1), 2) - pow((y2 - y1), 2));
-        return sqrt(pow((x2 - x1), 2) - pow((y2 - y1), 2));
+        this.length = sqrt(pow((x2 - x1), 2) - pow((y2 - y1), 2));
     }
 
-    public double getAngle(myLine line){
+    public void getAngle(myLine line){
         double x1 = line.getX();
         double x2 = this.EndX;
         double y1 = line.getY();
         double y2 = this.EndY;
-        return Math.atan2((x2 - x1), (y2 - y1));
+        this.angle = Math.atan2((x2 - x1), (y2 - y1));
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    @Override
+    public String toString(){
+        return "The length of the string is " + getLength() + " And the angle is " + getAngle();
     }
 
     @Override
@@ -41,6 +56,7 @@ public class myLine extends myShape{
         return line;*/
 
         gc.setStroke(getC().getARGB());
+        gc.setLineWidth(5.0);
         gc.strokeLine(getX(), getY(), EndX, EndY);
 
 
